f8f3fab65af60285049b0436fd6de2607bbca5eb
Sven <sven.preng@hva.nl>
2016-02-09 12:29:01 +0100

    Added some logic to process input

--- src/processlargedataset/ProcessLargeDataset.java
+++ src/processlargedataset/ProcessLargeDataset.java
@@ -19,11 +19,35 @@ public class ProcessLargeDataset {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws FileNotFoundException {
+        double assistentTotal;
+        double associateTotal;
+        double fullTotal;
+        double allTotal;
+
+        double assistentAverage;
+        double associateAverage;
+        double fullAverage;
+        double allAverage;
+
+        int assistent;
+        int associate;
+        int full;
+        int total;
 
         //Load file with salary
         try {
             java.io.File file = new java.io.File("Salary.txt");
             Scanner input = new Scanner(file);
+
+            while (input.hasNext()) {
+                String firstName = input.next();
+                String lastName = input.next();
+                String rank = input.next();
+                double salary = input.nextDouble();
+                System.out.println(firstName + " " + lastName + " " + rank + " " + salary);
+
+            }
+            input.close();
         } catch (Exception e) {
             System.out.println("File with the name specified can't be found");
         }
